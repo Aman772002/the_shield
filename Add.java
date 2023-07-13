4d1f491bb3a1edd052011d1a6d5d47a7eaa255b8
MOHIT MISHRA <131243943+Msh-2510@users.noreply.github.com>
2023-07-13 01:11:40 -0700

    Add files via upload

---
+++ Add.java
@@ -0,0 +1,10 @@
+public class  Add {
+    public static void main(String args[])
+    {
+        int a=10;
+        int b=20;
+        int total=a+b;
+        System.out.println(total);
+    }
+    
+}
